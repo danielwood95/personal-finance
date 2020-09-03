f9793c4d8bd560abb228bbadc457b03c3603b732
Daniel Wood <danielwood95@users.noreply.github.com>
2020-09-02 22:14:38 -0700

    Update savings.py

--- savings.py
+++ savings.py
@@ -1,24 +1,25 @@
 import math
 
-monthlySavings = 3500
-annualInterestRate = 7
-yearsSaving = 41
-annualSavingsIncrease = 2
+monthlyContribution = 1000   # How much you save each month
+annualInterestRate = 7      # Annual growth rate
+yearsSaving = 40            # Retirement age - current age
+annualSavingsIncrease = 2   # Annual rate of increase in monthly savings
+initialSavings = 10000
+totalSavings = initialSavings
 monthlyCompound = math.pow((1 + annualInterestRate/100.0),1/12.0)
-savings = 10000
+
 
 print "Total if you invest:"
 print '%12s  %12s  %12s' % ("YEAR", "TOTAL", "MONTHLY")
 print("------------------------------------------")
 for year in range(yearsSaving):
 	for months in range(12):
-		savings = monthlySavings + (savings * monthlyCompound)
-	print '%12s  %12s  %12s' % (year, int(savings), int(monthlySavings))
-	# print year, "\t", savings, "\t", monthlySavings
-	monthlySavings = monthlySavings * (1 + annualSavingsIncrease/100.0)
+		totalSavings = monthlyContribution + (totalSavings * monthlyCompound)
+	print '%12s  %12s  %12s' % (year, int(totalSavings), int(monthlyContribution))
+	monthlyContribution = monthlyContribution * (1 + annualSavingsIncrease/100.0)
 
 print("------------------------------------------\n")
 
-monthlySavings = 1000
-noInterest = yearsSaving * 12 * monthlySavings
-print "Total if you don't invest: ", noInterest
+monthlyContribution = 1000
+totalSavingsWithoutInvesting = yearsSaving * 12 * monthlyContribution
+print "Total if you don't invest: ", totalSavingsWithoutInvesting
